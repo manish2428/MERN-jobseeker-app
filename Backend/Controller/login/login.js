4f79071247b0bdb0cbe5ea9e83a8db5fc72bb77d
manish yadav <manishyadav2056107@gmail.com>
2022-07-06 10:15:11 +0530

    Login page resolved

--- Backend/Controller/login/login.js
+++ Backend/Controller/login/login.js
@@ -1,34 +1,33 @@
-require('dotenv').config()
-const Router=require("router")
-const router=Router()
-const bcrypt=require('bcrypt')
-const User=require('../../Model/singupschema')
+require("dotenv").config();
+const Router = require("router");
+const router = Router();
+const bcrypt = require("bcrypt");
+const User = require("../../Model/singupschema");
 
+router.get("/", (req, res) => {
+  res.status(200).json({ msg: "This is login page." });
+});
 
+router.post("/", async (req, res) => {
+  if (!(req.body.email && req.body.password)) {
+    return res.status(500).json({ err: "All fields are required" });
+  }
+  const email = req.body.email;
+  const password = req.body.password;
 
-router.get('/',(req,res)=>{
-    res.status(200).json({msg:"This is login page."})
-})
+  const data = await User.findOne({ email: email });
+  if (!data) {
+    return res.status(500).json({ msg: "User not found." });
+  }
 
-router.post('/',async(req,res)=>{
-    const {email,password}=req.body
-    const login= new User()
-    const data=await login.findOne({email:email})
-    if(!data){
-     return res.status(500).json({msg:"User not found."});
-    }
-    if(await bcrypt.compareSync(password,data.password)){
-        data.password="";
-       return res.status(200).send(data);
-    }
-    
-    
+  const auth = await bcrypt.compareSync(req.body.password, data.password);
+  if (auth) {
+    data.password = "";
+    return res.status(200).send(data);
+  }
+   else {
+    return res.status(500).json({ err: "Password didnot match." });
+  }
+});
 
-    
-
-
-   
-
-})
-
-module.exports=router;
+module.exports = router;
