702be4e7c50369062c3164441c6667149c3cf66e
manish yadav <manishyadav2056107@gmail.com>
2022-07-07 09:09:01 +0530

    profile post operation configured

--- Backend/Controller/profile/profile.js
+++ Backend/Controller/profile/profile.js
@@ -1,10 +1,24 @@
-const Profile=require("../../Model/profilemodel")
-const Router=require('router')
-const router=Router()
+const Profile = require("../../Model/profilemodel");
+const Router = require("router");
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.send("THIS IS PROFILE PAGE.");
+});
 
 
-router.get('/',(req,res)=>{
-    res.send("THIS IS PROFILE PAGE.");
-})
+// This is a post route for the profile where we post all the required fields 
+router.post("/", async (req, res) => {
+//   const {} = req.body;
+  const profile = new Profile(req.body);
+  await profile
+    .save()
+    .then(() => {
+      res.status(200).json({ msg: "Data saved successfully." });
+    })
+    .catch((e) => {
+      res.status(500).json({ error: e });
+    });
+});
 
-module.exports=router;
+module.exports = router;
