c47d8adcb5c02642f3523ea14fa2b517db14c11c
manish yadav <manishyadav2056107@gmail.com>
2022-07-07 10:10:26 +0530

    DELETE API CONGIGURED FOR THE PROFILE

--- Backend/Controller/profile/profile.js
+++ Backend/Controller/profile/profile.js
@@ -6,10 +6,9 @@ router.get("/", (req, res) => {
   res.send("THIS IS PROFILE PAGE.");
 });
 
-
-// This is a post route for the profile where we post all the required fields 
+// This is a post route for the profile where we post all the required fields
 router.post("/", async (req, res) => {
-//   const {} = req.body;
+  //   const {} = req.body;
   const profile = new Profile(req.body);
   await profile
     .save()
@@ -19,6 +18,20 @@ router.post("/", async (req, res) => {
     .catch((e) => {
       res.status(500).json({ error: e });
     });
+});
+
+router.delete("/del/:id", async(req, res) => {
+    if(!req.params.id){
+        res.status(500).send({errr:"ID should be passed as a parameter"})
+    }
+  try{
+    await Profile.deleteOne({ _id: req.params.id })
+    res.status(200).json({msg:"DATA DELETED SUCCESSFULLY"})
+  }
+  catch{
+    res.status(500).json({"err":"NO SUCH USER EXISTS"})
+  }
+   
 });
 
 module.exports = router;
